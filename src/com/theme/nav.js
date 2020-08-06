97fd1dbda6ba5586ce9426e184d4846829628415
ktsyr1 <46694587+ktsyr1@users.noreply.github.com>
2020-08-06 16:18:42 +0300

    418

--- src/com/theme/nav.js
+++ src/com/theme/nav.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react'   
 import {NavLink} from 'react-router-dom'
+import menu from '../../svg/Icon feather-menu.svg'
+import search from '../../svg/Icon awesome-search.svg'
+import logo from '../../svg/logo.svg'
 export default class Nav extends Component{ 
     render(){
         return(
             <nav className='nav'>
-               <NavLink to='/' className='logo'>id Tb</NavLink>
+                <img className='svg' src={menu} alt={"menu"}/>
+                <img className='svg' src={logo} alt={"logo"}/>
+                <img className='svg' src={search} alt={"search"}/>
             </nav>
         )
     }
