3af9550a043b58619e4c2bcb5afa4b2a16611598
ktsyr1 <46694587+ktsyr1@users.noreply.github.com>
2020-08-06 14:03:56 +0300

    203

--- src/com/theme/nav.js
+++ src/com/theme/nav.js
@@ -3,8 +3,8 @@ import {NavLink} from 'react-router-dom'
 export default class Nav extends Component{ 
     render(){
         return(
-            <nav className='box '>
-               <NavLink to='/'>id Tb</NavLink>
+            <nav className='nav'>
+               <NavLink to='/' className='logo'>id Tb</NavLink>
             </nav>
         )
     }
