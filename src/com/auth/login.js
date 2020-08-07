3d723611c46882e4de865e9223965fb3e6fc9794
ktsyr1 <46694587+ktsyr1@users.noreply.github.com>
2020-08-07 12:18:54 +0300

    bn

---
+++ src/com/auth/login.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+// import Itme from './itme'
+
+// import menu from '../../svg/Icon feather-menu.svg' 
+export default class Nav extends Component{ 
+    render(){
+        return(
+            <div className=''>
+                <div className='menu' >
+                    v
+                </div>
+            </div>
+        )
+    }
+}
