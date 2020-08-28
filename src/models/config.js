4083c49634a1d505809315edb492c2627e938144
ktsyr1 <46694587+ktsyr1@users.noreply.github.com>
2020-08-28 13:54:58 +0300

    Revert "Revert "11412""
    This reverts commit 4dabc44775adb7b448ca001fb5f1ac604f0a2c82.

--- src/models/config.js
+++ src/models/config.js
@@ -4,6 +4,7 @@ const URL= host
 exports.centers =  URL+`/api/v1/center/`
 exports.center =(x) =>  `${URL}/api/v1/center/${x}`
 exports.tables =(x) => `${URL}/api/v1/tables/${x}`
+exports.table =(x) => `${URL}/api/v1/table/${x}`
 exports.array = `${URL}/api/v1/array/`
 //auth
 exports.login = `${URL}/auth/v1/login`
